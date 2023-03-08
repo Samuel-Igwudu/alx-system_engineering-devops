de7f622592d919085e7ffef0982ed4ac7108fb29
Samuel-Igwudu <igwudu03@gmail.com>
2023-03-08 05:51:49 -0800

    0x15. API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" script that uses REST API """
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
+
+    user = '{}users/{}'.format(url, sys.argv[1])
+    res = requests.get(user)
+    json_f = res.json()
+    print("Employee {} is done with tasks".format(json_f.get('name')), end="")
+
+    todo = '{}todo?userId={}'.format(url, sys.argv[1])
+    res = requests.get(todo)
+    tasks = res.json()
+    NO_task = []
+    for task in tasks:
+        if task.get('completed') is True:
+            NO_task.append(task)
+
+    print("{}/{}):".format(len(NO_task), len(tasks)))
+    for task in NO_task:
+        print("\t {}".format(task.get("title")))
